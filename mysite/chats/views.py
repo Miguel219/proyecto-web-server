45b78c161448e95a4034cd21bdbfe39bffe4abee
Miguel219 <34550700+Miguel219@users.noreply.github.com>
2020-05-28 17:14:03 -0600

    Cambios

--- mysite/chats/views.py
+++ mysite/chats/views.py
@@ -51,7 +51,7 @@ class ChatViewSet(viewsets.ModelViewSet):
     def get_chatMessages(self, request, pk=None):
         
         chat = self.get_object()
-        chatMessages = Message.objects.filter(chat=chat.id).order_by('date')
+        chatMessages = Message.objects.filter(chat=chat.id).order_by('-date')
         if(chatMessages.count()>0):
             return(Response(MessageSerializer(chatMessages,many=True,context={'request':request}).data))
         else:
